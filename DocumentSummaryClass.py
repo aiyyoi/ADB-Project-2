ffe3d2821d3c95f5147943134dbae2a6afab111d
Akshaan Kakar <akshaan.crackers@gmail.com>
2015-10-29 16:03:42 -0400

    Added search to DocumentSummaryClass to retrieve top 4 docs for a query

--- DocumentSummaryClass.py
+++ DocumentSummaryClass.py
@@ -1,19 +1,57 @@
 import HashListClass
 from RulesReaderClass import *
+import urllib2
+import urllib
+import base64
+import json
+import time
 
 class DocumentSummary:
 
-	def __init__(self,classification):
+	def __init__(self,classification,host):
 		self.docFreqs = {}
 		self.nodeList = classification.split('/')
-		self.nodeList = list(reversed(self.nodeList))	
+		self.nodeList = list(reversed(self.nodeList))  
+		self.host = host
+                self.bingUrlBase = 'https://api.datamarket.azure.com/Bing/SearchWeb/v1/Composite?'
+                self.bingParams = {'$top': '4', '$format': 'json'}
+                #save your own key as key.json under the save path as this script
+                with open("./key.json") as key_file:
+                        data = json.load(key_file)
+                accountKey = data['accountKey']
+                # authentications
+                accountKeyEnc = base64.b64encode(accountKey + ':' + accountKey)
+                self.headers = {'Authorization': 'Basic ' + accountKeyEnc}
+
+
+
+
+
+
+
+
 
 
 	def generateSummaries(self):
 		for n in self.nodeList:
 			path = "./rules/" + n + ".txt"
-			queries = RulesReader(path).getRules()	
-			print(queries)	
+			queries = RulesReader(path).getRules()
+		
+
+
+
+	def search(self,query_string):
+		self.bingParams['Query'] = "'site:"+self.host+" "+query_string+"'" 
+	        bingUrl = self.bingUrlBase+urllib.urlencode(self.bingParams)
+                #wait
+                #time.sleep(1)
+		print(bingUrl)
+                req = urllib2.Request(bingUrl, headers = self.headers)
+                response = urllib2.urlopen(req)
+                content = json.loads(response.read())
+		print(content)
+
+
 
 
 
@@ -22,5 +60,6 @@ class DocumentSummary:
 
 
 ## TEST
-c = DocumentSummary("root")
+c = DocumentSummary("root","diabetes.org")
 c.generateSummaries()
+c.search()
